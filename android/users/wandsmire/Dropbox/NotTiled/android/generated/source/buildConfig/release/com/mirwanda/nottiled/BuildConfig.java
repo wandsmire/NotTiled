cefb4944a9a15e46cbf316e5e37df5e588266e93
WandsMire <reza@mirwanda.com>
2022-08-05 22:25:18 +0900

    release

--- android/users/wandsmire/Dropbox/NotTiled/android/generated/source/buildConfig/release/com/mirwanda/nottiled/BuildConfig.java
+++ android/users/wandsmire/Dropbox/NotTiled/android/generated/source/buildConfig/release/com/mirwanda/nottiled/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.mirwanda.nottiled";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 76;
-  public static final String VERSION_NAME = "1.7.1";
+  public static final int VERSION_CODE = 77;
+  public static final String VERSION_NAME = "1.7.2";
 }
